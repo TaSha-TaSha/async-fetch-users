db1a462daa32dd43258114765979c1c74f664126
塔莎 TaSha 塔莎 <96839389+TaSha-TaSha@users.noreply.github.com>
2022-09-13 14:37:36 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ class App extends Component {
     const response = await fetch(url);
     const date = await response.json();
     this.setState( {person: date.results[0]} )
-    // console.log(date.results[0]);
   }
   render() {
     return (
@@ -33,12 +32,3 @@ class App extends Component {
   }
 }
 export default App;
-
-
-
-// ПРИМЕЧАНИЕ:
-// Так как render() загружается ДО componentDidMount, тогда разумнее использовать 
-// тернарный оператор и сказать "до того как загрузится наш реквест покажи loading"
-// так как у нас несколько запросов, то разумнее использовать <div>
-// сам тернарный оператор не нужно ставить в тег, только в {}
-// не забываем удалять console.log()
