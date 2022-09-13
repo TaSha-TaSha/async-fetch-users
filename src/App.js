83fec29e2954764c5475bfef413eed552be63d64
macbook <macbook@MacBook-Pro-macbook.local>
2022-09-13 14:34:41 +0300

    seventh commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super();
 
+    this.state = {
+      person: null
+    }
+  }
+
+  async componentDidMount() {
+    const url = 'https://api.randomuser.me/';
+    const response = await fetch(url);
+    const date = await response.json();
+    this.setState( {person: date.results[0]} )
+    // console.log(date.results[0]);
+  }
+  render() {
+    return (
+      <div>
+        {!this.state.person ? <p>Loading ...</p> : 
+        <div>
+        <p>Your first name: {this.state.person.name.first}</p>
+        <p>Your second name: {this.state.person.name.last}</p>
+        <p>Your email: {this.state.person.email}</p>
+        <img src={this.state.person.picture.large} alt='users pic' width='300px' />
+        </div>
+        }
+      </div>
+    );
+  }
+}
 export default App;
+
+
+
+// ПРИМЕЧАНИЕ:
+// Так как render() загружается ДО componentDidMount, тогда разумнее использовать 
+// тернарный оператор и сказать "до того как загрузится наш реквест покажи loading"
+// так как у нас несколько запросов, то разумнее использовать <div>
+// сам тернарный оператор не нужно ставить в тег, только в {}
+// не забываем удалять console.log()
